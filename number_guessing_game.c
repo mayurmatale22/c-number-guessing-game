d3ff2a105dae66b5e481f4e9d71bfe4e8e5cb63b
mayurmatale22 <soappyyy42@gmail.com>
2026-09-13 16:47:43 +0530

    Refactor number guessing game logic
    Refactor guessing logic and improve output messages.

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -5,31 +5,37 @@
 
 int main(){
 
-int secret_number = 5;
-int guess = 0;
-int guessCount = 0;
-int guessLimit = 3;
-int outofguesses = 0;
+    int secret_number = 5;
+    int guess = 0;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outofguesses = 0;
 
-while ( guess != secret_number && outofguesses == 0){
-    if (guessCount < guessLimit){
+    while (guess != secret_number && outofguesses == 0){
 
-    
-    printf("Enter a number:");
-    scanf("%d" , &guess);
-    guessCount++;
+        if (guessCount < guessLimit){
+            printf("Enter a number: ");
+            scanf("%d", &guess);
+            guessCount++;
+
+            if (guess > secret_number){
+                printf("Too high!\n");
+            }
+            else if (guess < secret_number){
+                printf("Too low!\n");
+            }
+        }
+        else{
+            outofguesses = 1;
+        }
+    }
+
+    if (outofguesses == 1){
+        printf("Sorry, you're out of guesses.\n");
     }
     else{
-        outofguesses = 1;
+        printf("CONGRATS! YOU WIN!\n");
     }
-}
-if (outofguesses == 1){
-    printf("Sorry You're out of guesses\n");
-}
-else{
-printf("CONGRATS! YOU WIN!\n");
-}
-
 
     return 0;
 }
